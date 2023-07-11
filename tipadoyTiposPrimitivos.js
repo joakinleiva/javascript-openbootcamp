726954562a02448789f1ff9f32efbaa2a513cb9c
joakinleiva <joako.leiva9@gmail.com>
2023-07-11 03:40:42 -0400

    Tipado.js subido

---
+++ tipadoyTiposPrimitivos.js
@@ -0,0 +1,31 @@
+//Tipos primitivos
+
+//Number
+
+8;
+0;
+
+//string
+
+"Hola mundo"
+'Hola mundo'
+
+// Booleanos
+
+true;
+false;
+
+// Nulo y undefined
+
+null; //espacio asignado pero vacio
+undefined; //espacio que no esta definido 
+
+// null, undefined, false, 0
+
+if (true) {
+    console.log("Cumple");
+
+} else {
+    console.log("No cumple");
+
+}
